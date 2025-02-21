ce1f1709a2ad77e4f685f913675f4f54b00e7c77
Karol Gajda <jestemdobrywniczym@gmail.com>
2025-02-21 15:33:30 +0100

    complete client schemas

--- client/schemas.py
+++ client/schemas.py
@@ -4,14 +4,32 @@ from client.models import Client, ClientMachine, ClientPlace
 
 
 class ClientSchema(ModelSchema):
+    client_places: list[dict]
+
+    @staticmethod
+    def resolve_client_places(obj: Client) -> list[dict]:
+        client_places = obj.client_places.all()
+        return [ClientPlaceSchema.from_orm(client_place).dict() for client_place in
+                client_places] if client_places else []
+
     class Meta:
         model = Client
         fields = '__all__'
 
+
 class ClientPlaceSchema(ModelSchema):
+    client_machines: list[dict]
+
+    @staticmethod
+    def resolve_client_machines(obj: ClientPlace) -> list[dict]:
+        client_machines = obj.client_machines.all()
+        return [ClientMachineSchema.from_orm(client_machine).dict() for client_machine in
+                client_machines] if client_machines else []
+
     class Meta:
         model = ClientPlace
         fields = '__all__'
+
 
 class ClientMachineSchema(ModelSchema):
     class Meta:
