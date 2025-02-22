013cf3ab467ff3910e58a918ca50c9a984ab8361
Karol Gajda <jestemdobrywniczym@gmail.com>
2025-02-22 10:55:44 +0100

    update client machine

--- client/models.py
+++ client/models.py
@@ -7,30 +7,41 @@ class ClientMachine(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(TaskCategory, on_delete=models.CASCADE, blank=True, null=True)
+    client_place = models.ForeignKey("ClientPlace", on_delete=models.SET_NULL, related_name="machines", null=True, blank=True)
+    identifier = models.PositiveIntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = 'Client machine'
-        verbose_name_plural = 'Client machines'
+        verbose_name = "Client machine"
+        verbose_name_plural = "Client machines"
+        unique_together = ("client_place", "identifier")
+
+    def save(self, *args, **kwargs):
+        """ Automatically set `identifier` if not provided. """
+        if self.identifier is None:
+            last_machine = ClientMachine.objects.filter(client_place=self.client_place).order_by("identifier").last()
+            self.identifier = 1 if last_machine is None else last_machine.identifier + 1
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.name
+        return f"{self.client_place.name} - {self.name} (#{self.identifier})"
+
 
 class ClientPlace(models.Model):
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=100, blank=True, null=True)
     address = models.CharField(max_length=100, blank=True, null=True)
-    client_machines = models.ManyToManyField(ClientMachine, blank=True, related_name='machines')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = 'Client place'
-        verbose_name_plural = 'Client places'
+        verbose_name = "Client place"
+        verbose_name_plural = "Client places"
 
     def __str__(self):
         return self.name
+
 
 class Client(models.Model):
     name = models.CharField(max_length=100)
