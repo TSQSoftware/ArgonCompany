28b72a10e113b0e66a6fe39f77771d4302282fb3
Karol Gajda <jestemdobrywniczym@gmail.com>
2025-02-22 11:39:55 +0100

    add task attachment to admin site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from tasks.models import Task, TaskNote
+from tasks.models import Task, TaskNote, TaskAttachment
 
 admin.site.register(Task)
 admin.site.register(TaskNote)
+admin.site.register(TaskAttachment)
