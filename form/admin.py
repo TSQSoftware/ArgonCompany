1aa89909873cb308c42d3279f66ef796b0fe73b6
Karol Gajda <jestemdobrywniczym@gmail.com>
2025-02-26 16:55:13 +0100

    add displaying form question fields in admin

--- form/admin.py
+++ form/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from form.models import Form, FormAnswer, Question, Answer, QuestionCategory
+from form.models import Form, FormAnswer, Answer, QuestionCategory
 
 admin.site.register(Form)
 
@@ -13,16 +13,38 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = ('order',)
 
 
+from django.contrib import admin
+from .models import Question
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'form')
-    list_filter = ('category', 'form')
+    list_display = ('title', 'category', 'form', 'question_type', 'is_header', 'required', 'score', 'show_in_table')
+    list_filter = ('category', 'form', 'question_type', 'is_header', 'required', 'show_in_table')
+    search_fields = ('title', 'subtitle', 'category__name', 'form__name')
+
     fieldsets = (
         (None, {'fields': ('form', 'category')}),
         ('Question Details', {
-            'fields': ('title', 'subtitle', 'question_type', 'choices')
+            'fields': ('title', 'subtitle', 'question_type', 'choices', 'is_header', 'show_in_table'),
+        }),
+        ('Dependencies & Relations', {
+            'fields': ('depends_on', 'parent_question'),
+        }),
+        ('Validation & Scoring', {
+            'fields': ('required', 'validation_rules', 'score'),
+        }),
+        ('Additional Information', {
+            'fields': ('columns', 'media_url'),
         }),
     )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        for field_name in ['choices', 'columns', 'validation_rules']:
+            if field_name in form.base_fields:
+                form.base_fields[field_name].widget.attrs['style'] = 'width: 70%;'
+        return form
 
 
 admin.site.register(FormAnswer)
