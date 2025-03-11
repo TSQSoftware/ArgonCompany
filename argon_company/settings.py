4651a73b1715822bda3a22c3b40223f902a28b4f
Karol Gajda <jestemdobrywniczym@gmail.com>
2025-03-11 23:37:48 +0100

    fix

--- argon_company/settings.py
+++ argon_company/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'argon_company.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/media', 'db.sqlite3'),
     }
 }
 
